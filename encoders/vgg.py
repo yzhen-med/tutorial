02482b5fbb0ad624d868b016a803dd91079bc5a3
yzhen-med <yzhen.med@gmail.com>
2025-05-22 11:15:26 +0800

    [mod] 重构unet结构

--- encoders/vgg.py
+++ encoders/vgg.py
@@ -1,4 +1,6 @@
-""" VGG """
+""" VGG: N * (Conv => ReLU) => MaxPool
+see paper: 
+see codes: https://github.com/rasbt/deeplearning-models/blob/master/pytorch_ipynb/cnn/cnn-vgg19.ipynb """
 
 import torch
 import torch.nn as nn
@@ -8,80 +10,15 @@ from blocks.convs import ConvNormAct, ConvAct
 from functools import partial
 
 
-# ==== Settings ==== #
-CONV = nn.Conv2d
-NORM = nn.BatchNorm2d
-ACT  = nn.ReLU
-ConvAct = partial(ConvAct, CONV=CONV, ACT=ACT)
-ConvNormAct = partial(ConvNormAct, CONV=CONV, NORM=NORM, ACT=ACT)
-# ================== #
-
-
-class InConv(nn.Module):
-    """ Conv => Act """
-    def __init__(self, c_in, c_out, k=7, s=1, p=3, d=1, g=1):
+class Block(nn.Module):
+    """ N * Conv """
+    def __init__(self, c_in, c_out, k=3, s=1, p=1, d=1, g=1, depth=2,
+                 conv_layer=ConvAct):
         super().__init__()
-        self.conv = ConvAct(c_in, c_out, k, s, p, d, g)
-
-    def forward(self, img):
-        zs = [self.conv(img)]
-        return zs
-
-
-class InConvMultiScale(InConv):
-    """ 多尺度图像输入 """
-    def forward(self, img, multi_scale_imgs):
-        zs = [self.conv(img)]
-        for v in multi_scale_imgs:
-            zs.append(self.conv(v))
-        return zs
-
-
-class Down(nn.Module):
-    """ (Downsample => N*Conv) """
-    def __init__(self, c_in, c_out, k=3, s=1, p=1, d=1, g=1, depth=2):
-        super().__init__()
-        self.down  = CONV(c_in, c_in, 2, 2, 0)
         self.layer = nn.Sequential(
-            ConvNormAct(c_in, c_out, k, s, p, d, g),
-            *[ConvNormAct(c_out, c_out) for _ in range(1, depth)])
+            conv_layer(c_in, c_out, k, s, p, d, g),
+            *[conv_layer(c_out, c_out) for _ in range(1, depth)])
 
     def forward(self, x):
-        z = self.down(x)
-        z = self.layer(z)
-        return z
-
-
-class Encoder(nn.Module):
-    def __init__(self, in_channel, in_out, depths):
-        super().__init__()
-        self.in_conv = InConv(in_channel, in_out[0][0])
-        self.layers = nn.ModuleList()
-        for (c_in, c_out), depth in zip(in_out, depths):
-            self.layers.append(Down(c_in, c_out, depth=depth))
-
-    def forward(self, img):
-        zs = self.in_conv(img)
-        for i, layer in enumerate(self.layers):
-            z = layer(zs[i])
-            zs.append(z)
-        return zs
-
-
-class EncoderMultiScale(nn.Module):
-    def __init__(self, in_channel, in_out, depths):
-        super().__init__()
-        self.in_conv = InConvMultiScale(in_channel, in_out[0][0])
-        self.layers = nn.ModuleList()
-        for (c_in, c_out), depth in zip(in_out, depths):
-            self.layers.append(Down(c_in, c_out, depth=depth))
-
-    def forward(self, img, multi_scale_imgs):
-        zs = self.in_conv(img, multi_scale_imgs)
-        z = zs[0]
-        for i, layer in enumerate(self.layers):
-            if (i > 0) and (i <= len(multi_scale_imgs)):
-                z = torch.cat([zs[i], zs[-1]], dim=1)
-            z = layer(z)
-            zs.append(z)
-        return zs
+        x = self.layer(x)
+        return x
---
+++ models/unet.py
@@ -0,0 +1,85 @@
+"""
+分割模型框架
+UNet
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from copy import deepcopy
+from functools import partial
+from blocks.convs import ConvNormAct, ConvAct
+from encoders.vgg import Block as vgg_block
+
+
+# ==== Settings ==== #
+CONV = nn.Conv2d
+NORM = nn.BatchNorm2d
+ACT  = nn.ReLU
+ConvAct = partial(ConvAct, CONV=CONV, ACT=ACT)
+ConvNormAct = partial(ConvNormAct, CONV=CONV, NORM=NORM, ACT=ACT)
+vgg_block = partial(vgg_block, conv_layer=ConvNormAct)
+# ================== #
+
+
+class InConv(ConvAct):
+    """ Conv => Act """
+    def forward(self, img, multi_scale_imgs=[]):
+        zs = [self.layer(img)]
+        if len(multi_scale_imgs):
+            for v in multi_scale_imgs:
+                zs.append(self.layer(v))
+        return zs
+
+
+class Encoder(nn.Module):
+    def __init__(self, in_channel, in_out, depths):
+        super().__init__()
+        self.in_conv = self.build_in_conv(in_channel, in_out[0][0])
+        self.enc_layers = nn.ModuleList()
+        for (c_in, c_out), depth in zip(in_out, depths):
+            self.enc_layers.append(nn.Sequential(
+                CONV(c_in, c_in, kernel_size=2, stride=2, padding=0),  # downsample
+                vgg_block(c_in, c_out, depth=depth)))
+
+    def build_in_conv(self, c_in, c_out):
+        return InConv(c_in, c_out)
+
+    def forward(self, x):
+        enc_zs = self.in_conv(x)
+        for layer in self.enc_layers:
+            x = layer(enc_zs[-1])
+            enc_zs.append(x)
+        return enc_zs
+
+
+class EncoderMultiScale(Encoder):
+    def forward(self, x, multi_scale_imgs):
+        enc_zs = self.in_conv(x, multi_scale_imgs)[::-1]
+        N = len(multi_scale_imgs)
+        for i, layer in enumerate(self.enc_layers):
+            x = enc_zs[-1]
+            if 0 < i <= N:
+                x = torch.cat([x, enc_zs[N - i]], dim=1)
+            x = layer(x)
+            enc_zs.append(x)
+        return enc_zs
+
+
+class Decoder(nn.Module):
+    def __init__(self, in_out, depths):
+        super().__init__()
+        self.dec_layers = nn.ModuleList()
+        self.up = nn.Upsample(scale_factor=2, align_corners=True, mode='bilinear')
+        for (c_in, c_out), depth in zip(in_out, depths):
+            self.dec_layers.append(nn.Sequential(
+                vgg_block(c_in, c_out, depth=depth)))
+
+    def forward(self, enc_zs):
+        dec_zs = []
+        z = enc_zs.pop()
+        for i, layer in enumerate(self.dec_layers):
+            z = torch.cat([self.up(z), enc_zs.pop()], dim=1)
+            z = layer(z)
+            dec_zs.append(z)
+        return dec_zs
